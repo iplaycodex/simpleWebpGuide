bda1e193a5ab5b2f8a18c5074a802388664241cc
liuliang <liuliang@111.com.cn>
2020-06-25 14:59:57 +0800

    update project

---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="hello">
+        <h4>custom img component support webp type</h4>
+        <webp-img :path="img" />
+        <webp-img :path="require('../assets/pic2.jpg')" />
+    </div>
+</template>
+
+<script>
+import WebpImg from '../components/WebpImg';
+export default {
+    data() {
+        return {
+            img: require('../assets/pic1.jpg')
+        }
+    },
+    components: {
+        WebpImg
+    }
+}
+</script>
+
+<style scoped>
+.hello {
+    width: 100%;
+}
+.hello img {
+    width: 100%;
+}
+</style>
